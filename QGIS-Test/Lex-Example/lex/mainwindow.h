e27b69dbbaeea53ea8240558f7659c1cb4992f89
serdarkocerr <49731780+serdarkocerr@users.noreply.github.com>
2022-06-26 01:10:36 +0300

    maptools added.

--- QGIS-Test/Lex-Example/lex/mainwindow.h
+++ QGIS-Test/Lex-Example/lex/mainwindow.h
@@ -16,10 +16,40 @@
 #include <qgsproject.h>
 #include <qgssymbol.h>
 #include <QVBoxLayout>
+#include <qgsrulebasedrenderer.h>
+#include <qgsmaptool.h>
+#include <qgsmapmouseevent.h>
+#include <qgsmaptoolidentify.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+class PanTool : public QgsMapTool {
+
+public:
+
+   PanTool(QgsMapCanvas *canvas);
+
+
+
+   // QgsMapTool interface
+public:
+   void canvasMoveEvent(QgsMapMouseEvent *e);
+   void canvasReleaseEvent(QgsMapMouseEvent *e);
+};
+
+class ExploreTool : public QgsMapToolIdentify{
+public:
+
+    ExploreTool(QgsMapCanvas *canvas);
+
+
+
+    // QgsMapTool interface
+public:
+    void canvasReleaseEvent(QgsMapMouseEvent *e);
+};
 
 class MainWindow : public QMainWindow
 {
@@ -30,11 +60,32 @@ public:
     ~MainWindow();
 
     void loadMap();
+    void arrangeLandMarks(QgsMapLayer *mapLayer,QgsRuleBasedRenderer *renderer,QgsSymbol *symbol);
+    void createAndConnectToolbar();
+    void setPanMode();
+    void setExploreMode();
+public slots:
+    void showBaseMapLayerSLOT();
+    void showLandMapLayerSLOT();
+    void zoomInSLOT();
+    void zoomOutSLOT();
+    void setPanModeSLOT();
+    void setExploreModeSLOT();
 
 private:
     QgsMapCanvas* mapCanvas;
     QVBoxLayout *playout;
+    PanTool *panTool;
+    ExploreTool *exploreTool;
+    QAction *actionPan;
+    QAction *actionExplore;
+    QAction *actionShowBaseMapLayer;
+    QAction *actionShowLandmarkLayer;
+    QgsVectorLayer *lVector;
+    QgsRasterLayer *lRaster;
     Ui::MainWindow *ui;
 
 };
+
+
 #endif // MAINWINDOW_H
